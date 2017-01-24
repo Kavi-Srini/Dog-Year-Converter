4ced53cc99fa74c4661d8a4e9dea16bd061cfb3a
Kavitha Srinimukesh <kavitha.srinimukesh@gmail.com>
2017-01-24 18:08:08 -0500

    Initial Commit

---
+++ Dog Year Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dog Year Converter
+//
+//  Created by Nachammai K Srinimukesh on 1/24/17.
+//  Copyright © 2017 Nachammai K Srinimukesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Dog Year ConverterTests/Dog_Year_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dog_Year_ConverterTests.swift
+//  Dog Year ConverterTests
+//
+//  Created by Nachammai K Srinimukesh on 1/24/17.
+//  Copyright © 2017 Nachammai K Srinimukesh. All rights reserved.
+//
+
+import XCTest
+@testable import Dog_Year_Converter
+
+class Dog_Year_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
